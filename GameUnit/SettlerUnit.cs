32cb6b696217ef6667400fb0c08ea02580c4b787
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-04-08 18:39:46 +0100

    Classe SettlerUnit

---
+++ GameUnit/SettlerUnit.cs
@@ -0,0 +1,13 @@
+namespace GameUnit
+{
+    public class SettlerUnit : Unit // Heranca!!
+    {
+        
+
+        public SettlerUnit(int mov, int health, int attackPower) : base(mov, health)
+        {                                                        // Unit(int, int)
+           
+        }     
+    }
+
+}

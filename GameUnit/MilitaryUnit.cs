cdb7e0ca15498660a50471b12fce7e2cec308cd3
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-04-08 18:01:34 +0100

    More work on 5 MU

--- GameUnit/MilitaryUnit.cs
+++ GameUnit/MilitaryUnit.cs
@@ -10,5 +10,29 @@ namespace GameUnit
             AttackPower = attackPower;
             XP = 0;
         }
+    
+        //Metodo public void
+        public void Attack(Unit u)
+        {   
+            XP = ++XP;
+            int dano = AttackPower + XP;
+
+        }
+
+
+        
+    
+    
     }
 }
+
+/*
+Faz override de Health tal que esta seja igual à Health base + XP
+Faz override de Cost, tal que retorne AttackPower + XP
+Cria método public void Attack(Unit u) que incrementa o XP da própria unidade e aplica um dano igual a AttackPower na Health
+da unidade atacada
+
+
+
+
+*/
